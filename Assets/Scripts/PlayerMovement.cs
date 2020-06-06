6727c7d59b9cd4767ba679c80274ff50375f7a0f
Xaitlyn <essiieer@hotmail.com>
2020-06-06 12:26:22 +0200

    Correr y Plataforma de salto
    Ahora hay una variable que guarda el valor de speed actual, y ya tenemos la plataforma de salto

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,21 +9,29 @@ public class PlayerMovement : MonoBehaviour
     //speed de movimiento normal
     public float speedWalk = 14f;
     //speed de movimiento corriendo
-    public float speedRun = 20f;
+    public float speedRun = 24f;
+    //guardamos la speed aqui
+    private float actualSpeed;
     //fuerza de la gravedad constante
     public float gravity = -19.62f;
-
+    //altura del salto
     public float jumpHeight = 3f;
+    //altura del salto en la plataforma
+    public float jumpPlatformHeight = 10f;
 
     //referencia al gameobject para chequear si estamos tocando suelo, la distancia entre el suelo y el player y la LayerMask
     public Transform GroundCheck;
     public float groundDistance = 0.1f;
     public LayerMask groundMask;
 
+    public LayerMask jumpPlatformMask;
+
 
     Vector3 velocity;
     //bool para saber si estamos tocando el suelo o no
     bool isGrounded;
+    //bool para saber si estamos tocando la plataforma de salto
+    bool isInJumpBoost;
     bool iHave2jump;
 
     void Start()
@@ -41,6 +49,12 @@ public class PlayerMovement : MonoBehaviour
             iHave2jump = true;
         }
 
+        isInJumpBoost = Physics.CheckSphere(GroundCheck.position, groundDistance, jumpPlatformMask);
+        if(isInJumpBoost && velocity.y <0)
+        {
+            velocity.y = Mathf.Sqrt(jumpPlatformHeight * -2f * gravity);
+        }
+
         //esto pilla referencia a si nos estamos moviendo hacia adelante o atras
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
@@ -48,7 +62,15 @@ public class PlayerMovement : MonoBehaviour
         //se encarga de mirar hacia donde estamos moviendonos
         Vector3 move = transform.right * x + transform.forward * z;
         //se mueve depende de las teclas que pillemos + la speed
-        controller.Move(move * speedWalk * Time.deltaTime);
+        if(Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+        {
+            actualSpeed = speedRun;
+        }
+        else
+        {
+            actualSpeed = speedWalk;
+        }
+        controller.Move(move * actualSpeed * Time.deltaTime);
 
         //salto
         if (Input.GetButtonDown("Jump")&& isGrounded)
